ef807d5b8221e2e2eaa14407ccac8fa55a833e8c
Yohannes Takata <yohannestakata1@gmail.com>
2024-05-14 03:34:08 +0300

    Add project files.

---
+++ Temperature Converter/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(183, 131)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(66, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Celsius"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(457, 128)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(93, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Fahrenheit"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 14F)
+        TextBox1.Location = New Point(183, 159)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 45)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 14F)
+        TextBox2.Location = New Point(457, 156)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 45)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(339, 156)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 4
+        Button1.Text = "<->"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(339, 219)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 5
+        Button2.Text = "Convert"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 16F)
+        Label3.Location = New Point(228, 41)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(347, 45)
+        Label3.TabIndex = 6
+        Label3.Text = "Temperature Converter"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ Temperature Converter/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Dim mode As Char = "c"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If mode = "c" Then
+            mode = "f"
+            Label1.Text = "Fahrenheit"
+            Label2.Text = "Celsius"
+        Else
+            mode = "c"
+            Label1.Text = "Celsius"
+            Label2.Text = "Fahrenheit"
+        End If
+
+        Dim temp As String
+
+        temp = TextBox1.Text
+        TextBox1.Text = TextBox2.Text
+        TextBox2.Text = temp
+    End Sub
+
+    Function ToCelsius(input As Double) As Double
+        Return (input - 32) * 5 / 9
+    End Function
+
+    Function ToFahrenheit(input As Double) As Double
+        Return (9 / 5) * input + 32
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If mode = "f" Then
+            TextBox2.Text = ToCelsius(Double.Parse(TextBox1.Text))
+        Else
+            TextBox2.Text = ToFahrenheit(Double.Parse(TextBox1.Text))
+        End If
+    End Sub
+End Class
